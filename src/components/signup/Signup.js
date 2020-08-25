cf2028dd34ae46f642526b7ef52698ba50d0bea7
Aayushi Somani <30904417+asomani27@users.noreply.github.com>
2020-08-26 00:19:03 +0530

    Update Signup.js

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -83,7 +83,7 @@ class Signup extends React.Component {
             <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
             <input class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" onChange={this.onNameChange} placeholder="Name" type="text" />
             <input class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" onChange={this.onEmailChange} placeholder="Username" type="text" />
-            <input class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" onChange={this.onPasswordChange} placeholder="Password" type="text" />
+            <input class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" onChange={this.onPasswordChange} placeholder="Password" type="password" />
             <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={this.signup} >Sign up</button>
           </div>
         </div>
